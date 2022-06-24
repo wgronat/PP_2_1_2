c84095a1c9ca00874f4a473edb37eadbf10e0a46
wgronat <ildebote@bk.ru>
2022-06-24 11:53:27 +0500

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld been2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beenCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beenCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean1.equals(been2));
+        System.out.println(beenCat1.equals(beenCat2));
     }
 }
